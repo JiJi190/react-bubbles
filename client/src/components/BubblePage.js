39d8346fb3b39e38c2c355e57d9ae4b52d7695f1
unknown <robertrey1234@gmail.com>
2019-09-20 12:13:13 -0500

    Not recieving token

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import axiosWithAuth from '../auth/axiosWithAuth'
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,7 +9,15 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+useEffect(() => getData(), []);
+function getData() {
+  axiosWithAuth()
+  .get("http://localhost:5000/api/colors")
+  .then(res => {
+    setColorList(res.data);
+  })
+  .catch(err => console.log(err.response))
+}
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
